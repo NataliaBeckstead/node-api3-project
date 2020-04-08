a341f27ce32ca58013bc22c978c247808b7148e9
Natalia Beckstead <toosmiravilla@gmail.com>
2020-04-08 15:19:32 -0600

    user router

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const users = require("./userDb");
 
 const router = express.Router();
 
 router.post('/', (req, res) => {
-  // do your magic!
+  users.insert(req.body)
+    .then(() => {
+      res.status(201).json(req.body);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        messge: "There was an error while saving the post to the database",
+    });
+  });
 });
 
 router.post('/:id/posts', (req, res) => {
@@ -11,7 +21,14 @@ router.post('/:id/posts', (req, res) => {
 });
 
 router.get('/', (req, res) => {
-  // do your magic!
+  users.get(req.query)
+    .then((posts) => {
+      res.status(200).json(posts);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ message: "Error retrieving posts" });
+  });
 });
 
 router.get('/:id', (req, res) => {
@@ -19,15 +36,36 @@ router.get('/:id', (req, res) => {
 });
 
 router.get('/:id/posts', (req, res) => {
-  // do your magic!
+  users.getUserPosts(req.params.id)
+    .then((posts) => {
+      if (posts) {
+        res.status(200).json(posts);
+      } else {
+        res.status(404).json({ message: "post not found" });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ message: "Error processing request" });
+  });
 });
 
 router.delete('/:id', (req, res) => {
-  // do your magic!
+  users.remove(req.params.id)
+    .then((user) => res.status(200).json(user))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: "The user could not be removed" });
+  });
 });
 
 router.put('/:id', (req, res) => {
-  // do your magic!
+  users.update(req.params.id, req.body)
+    .then(res.status(200).json(req.body))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: "Error processing request" });
+  });
 });
 
 //custom middleware
