2434eca54350d4783913df3f013e326c8d393968
Natalia Beckstead <toosmiravilla@gmail.com>
2020-04-08 14:36:44 -0600

    logger and connected routers

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 
+const userRouter = require("./users/userRouter.js");
+const postRouter = require("./posts/postRouter.js");
+
 const server = express();
+
+server.use(logger);
+
+server.use("/api/user", userRouter);
+server.use("/api/post", postRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +16,9 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`[${new Date().toISOString()}] ${req.method} Request to ${req.originalUrl}`);
+  next();
+}
 
 module.exports = server;
